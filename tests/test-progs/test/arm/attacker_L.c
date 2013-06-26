d141a29dfcc1a5801d7be06a953a376cfdeecb47
Yao Wang <yw4382@cornell.edu>
2013-06-26 16:59:54 -0400

    source files for test programs

---
+++ tests/test-progs/test/arm/attacker_L.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int i, j;
+	int * buffer;
+	buffer = (int *) malloc(sizeof(int)*1000);
+	
+	for (i = 0; i < 1000; i++) 
+	{
+		for (j = 0; j < 50; j++)
+			buffer[i] = i;
+	}
+
+	printf ("Finish initialization\n");
+	
+	return 0;
+}
---
+++ tests/test-progs/test/arm/victim.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int i;
+	int * buffer;
+	buffer = (int *) malloc(sizeof(int)*1000);
+	
+	for (i = 0; i < 1000; i++) buffer[i] = i;
+
+	printf ("Finish initialization\n");
+	
+	return 0;
+}
