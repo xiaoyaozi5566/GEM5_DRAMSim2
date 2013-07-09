b5497e3dc0edda659cf1626cc1493f9b4b4b8f10
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2013-07-09 16:32:16 -0400

    Changed attacker_H. Changed Makefile.

--- tests/test-progs/test/arm/attacker_H.c
+++ tests/test-progs/test/arm/attacker_H.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 
 int main() {
-	int i, j;
-	int * buffer;
-	buffer = (int *) malloc(sizeof(int)*1000);
-	
-	for (i = 0; i < 1000; i++) buffer[i] = i;
-	
-	return 0;
+    int i, j;
+    int * buffer;
+    buffer = (int *) malloc(sizeof(int)*1000);
+
+
+    for (i = 0; i < 1000; i++) buffer[i] = i;
+    while(1){}
+
+    return 0;
 }
