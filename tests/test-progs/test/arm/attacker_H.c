4013543b1bf4e900b1d859c6595d613b442966b1
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2013-12-11 19:47:26 -0500

    Benchmark with arithmetic nop instead of usleep

--- tests/test-progs/test/arm/attacker_H.c
+++ tests/test-progs/test/arm/attacker_H.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 int main() {
 	int i, j;
 	int * buffer;
 	buffer = (int *) malloc(sizeof(int)*2000);
-	
 	for (i = 0; i < 2000; i++) 
 	{
 		for (j = 0; j < 2; j++)
 			buffer[i] = i;
 	}
-	
 	return 0;
 }
--- tests/test-progs/test/arm/randmem.c
+++ tests/test-progs/test/arm/randmem.c
@@ -3,11 +3,17 @@
 #include <unistd.h>
 #include <ctype.h>
 
+void nop( int delay ){
+    int tmp,i;
+    for( i=0; i<delay; i++ )
+        tmp = i + tmp;
+}
+
 int main(int argc, char **argv) {
     int SEED = 2;
     int MEM_SIZE = 1024 * 1024; // *32 bits (number of ints)
     int DURATION = 6 * 1000 * 1000; //us
-    int MEM_PERIOD = 1000; //us between mem requests
+    int DELAY_OPS = 1000; //us between mem requests
 
     int opt;
     while( ( opt = getopt( argc, argv, "s:d:p:" ) ) != -1 ){
@@ -19,15 +25,12 @@ int main(int argc, char **argv) {
                 DURATION = atoi(optarg); // seconds
                 break;
             case 'p':
-                MEM_PERIOD = atoi(optarg); // us
+                DELAY_OPS = atoi(optarg); // us
                 break;
             default:
                 break;
         }
     } 
-
-    printf("size: %d bits, period:%d us, duration:%d us\n",MEM_SIZE/32,MEM_PERIOD,DURATION);
-
 
     int * mem = ( int* ) malloc( sizeof( int ) * MEM_SIZE );
     int elapsed = 0;
@@ -37,9 +40,9 @@ int main(int argc, char **argv) {
     while( elapsed < DURATION ){
         int read_addr  = rand() % MEM_SIZE;
         int write_addr = rand() % MEM_SIZE;
-        mem[write_addr]= mem[read_addr];
-        elapsed += MEM_PERIOD;
-        usleep(MEM_PERIOD);
+        tmp = mem[read_addr];
+        elapsed += DELAY_OPS;
+        nop(DELAY_OPS);
     }
 
 }
