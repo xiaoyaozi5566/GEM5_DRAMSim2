ae3776b5c9a705f7df38fc7965effb4e291e78e1
root <root@master.(none)>
2014-01-08 20:51:49 +0000

    To run without fast forwarding global_exit_count needs to start at 0

--- src/sim/global_exit_count.hh
+++ src/sim/global_exit_count.hh
@@ -5,7 +5,7 @@ class ExitCounter{
     private:
         // If fastforwarding is turned of the count
         // needs to be manually changed to 0 here.
-        ExitCounter(){ count=1; };
+        ExitCounter(){ count=0; };
         static ExitCounter * instance;
         int count;
 
