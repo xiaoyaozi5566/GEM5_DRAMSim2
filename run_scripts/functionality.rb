07c5c7a2e1d109349bd50dfc71912f68528db291
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2014-04-30 12:58:39 -0400

    A functionality test with fewer, longer tests

--- run_scripts/functionality.rb
+++ run_scripts/functionality.rb
@@ -9,11 +9,32 @@ module RunScripts
 
     def test_sanity
 
-        cpus = $cpus # %w[ timing ]
-        schemes = $schemes #w%w[ tp ]
-        benchmarks = $specint #%w[ mcf sjeng ]
+        cpus = $cpus
+        schemes = $schemes
+        benchmarks = $specint
         opts = {
             maxinsts: 10**3,
+            fastforward: 100,
+            runmode: :local,
+        }
+
+        cpus.product( schemes ).each do |cpu, scheme|
+            Parallel.each( benchmarks.combination( 2 ),
+                          in_threads: 4 ) do |p0, p1|
+                iteropts = { p1: p1 }
+                sav_script( cpu, scheme, p0, opts.merge( iteropts ) )
+            end
+        end
+
+    end
+
+    def test_sanity_deep
+
+        cpus = %w[ detailed ]
+        schemes = $schemes 
+        benchmarks = %w[ mcf sjeng ]
+        opts = {
+            maxinsts: 10**6,
             fastforward: 100,
             runmode: :local,
         }
