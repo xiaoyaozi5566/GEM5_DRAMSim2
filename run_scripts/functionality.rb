acdfb5422f752e3c1949576050a0710246a32e61
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2014-05-08 09:54:26 -0400

    Functional tests cleaned up. Tests for rr_nc / full system

--- run_scripts/functionality.rb
+++ run_scripts/functionality.rb
@@ -7,21 +7,21 @@ require 'colored'
 include RunScripts
 
 module RunScripts
-    def test_sanity_ opts
-        cpus = $cpus
-        schemes = %w[ none tp ]
-        benchmarks = $specint
+    def sanity_ opts
         opts = {
+            cpus: $cpus,
+            schemes: %w[ none tp ],
+            benchmarks: $specint,
             maxinsts: 10**3,
             fastforward: 100,
             runmode: :local,
             debug: true,
-        }
+        }.merge opts
 
         failed = []
 
-        cpus.product( schemes ).each do |cpu, scheme|
-            Parallel.each( benchmarks.combination( 2 ),
+        opts[:cpus].product( opts[:schemes] ).each do |cpu, scheme|
+            Parallel.each( opts[:benchmarks].combination( 2 ),
                           in_threads: 4 ) do |p0, p1|
                 iteropts = { p1: p1 }
                 r = sav_script( cpu, scheme, p0, opts.merge( iteropts ) )
@@ -31,65 +31,52 @@ module RunScripts
         failed.each{|f| puts f.red}
     end
 
-    def test_sanity
+    def sanity_deep_ opts
+
         opts = {
-            maxinsts: 10**3,
-            fastforward: 100,
-            runmode: :local,
-            debug: true,
-        }
-        test_sanity_ opts
-    end
-
-    def test_sanity_deep_ opts
-        cpus = %w[ detailed ]
-        schemes = %w[ fa none tp ]
-        benchmarks = %w[ mcf sjeng ]
-
-        failed = []
-
-        cpus.product( schemes ).each do |cpu, scheme|
-            Parallel.each( benchmarks.combination( 2 ),
-                          in_threads: 4 ) do |p0, p1|
-                iteropts = { p1: p1 }
-                r = sav_script( cpu, scheme, p0, opts.merge( iteropts ) )
-                failed << r[1] unless r[0]
-            end
-        end
-
-        failed.each{|f| puts f.red}
-    end
-
-    def test_sanity_deep
-        opts = {
+            cpus: %w[ detailed ],
+            schemes: %w[ none tp fa ],
+            benchmarks: %w[ mcf sjeng ],
             maxinsts: 10**6,
             fastforward: 100,
             runmode: :local,
             debug: true,
-        }
-        test_sanity_deep_ opts
+        }.merge opts
+
+        sanity_( opts )
+
     end
 
-    def test_set_part
-        opts = {
-            maxinsts: 10**3,
-            fastforward: 100,
-            runmode: :local,
-            debug: true,
-            setpart: true,
-        }
-        test_sanity_ opts
+    def sanity
+        sanity_( {} )
     end
 
-    def test_set_part_deep
-        opts = {
-            maxinsts: 10**6,
-            fastforward: 100,
-            runmode: :local,
-            debug: true,
-            setpart: true,
-        }
-        test_sanity_deep_ opts
+    def sanity_deep
+        sanity_deep_( {} )
+    end
+
+    def set_part
+        sanity_( {setpart: true } )
+    end
+
+    def set_part_deep
+        sanity_deep_( {setpart: true} )
+    end
+
+    def rr_nc
+        sanity_( {rr_nc: true} )
+    end
+
+    def rr_nc_deep
+        sanity_deep_( {rr_nc: true} )
+    end
+
+    def secure
+        sanity_( { setpart: true, rr_nc: true } )
+    end
+
+    def secure_deep
+        sanity_deep_( { setpart: true, rr_nc: true } )
     end
 
 end
