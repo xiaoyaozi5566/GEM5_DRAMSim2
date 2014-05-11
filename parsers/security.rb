c782fc9db59ff8bfbda85b5c52583aadb5030bd4
Andrew <andrew@csl.cornell.edu>
2014-05-10 20:41:28 -0400

    Parser gets distribution of cycle count difference

--- parsers/security.rb
+++ parsers/security.rb
@@ -18,7 +18,7 @@ def compare_etime( conf = {} )
             hsh[p1] = time if found
             hsh
         end
-        conf[:bench].product( conf[:bench] ).each do |p1i,p1j|
+        conf[:bench].combination(2) do |p1i,p1j|
             if times[p1i] != times[p1j]
                 fi = stdo_file( p.merge( {p1:p1i } ) )
                 fj = stdo_file( p.merge( {p1:p1j } ) )
@@ -38,30 +38,55 @@ def avg_difference conf
     differences.inject(:+)/differences.length
 end
 
-def avg_percent conf
-    diff_percents = []
-    compare_etime( conf ){|t1,t2|
+def percent_diff(t1,t2)
         unless t1.nil? || t2.nil?
             high = ( t1>=t2 && t1 ) || ( true && t2 ) 
             low  = ( t1>=t2 && t2 ) || ( true && t1 )
-            diff_percents << (high-low)/high * 100
+            (high-low)/((high+low)/2) * 100
         end
+end
+
+def avg_percent conf
+    diff_percents = []
+    compare_etime( conf ){|t1,t2|
+            diff = method(:percent_diff).call(t1,t2)
+            diff_percents << diff unless diff.nil?
     }
-    diff_percents.inject(:+)/diff_percents.length
+    diff_percents.sort.reverse.inject(:+)/diff_percents.length
+end
+
+def percent_diff_dist conf
+    dist=Hash.new(0)
+    tests = [  ->(x){x <= 1 } ]
+    (1..11).each{|i| tests << ->(x){ x >= i && x< i+1} }
+    tests << ->(x){ x >= 12 }
+    compare_etime( conf ){|t1,t2|
+        diff = method(:percent_diff).call(t1,t2)
+        dist[tests.map{|l| l.call( diff ) }.index(true)]+=1 unless diff.nil?
+    }
+    dist
+end
+
+def count_differences conf
+    diffs = 0
+    compare_etime( conf ){|t1,t2| diffs+=1 }
+    diffs
 end
 
 if __FILE__ == $0
     result_dir = ARGV[0].to_s
 
-    %w[ tp ].each do |scheme|
+    %w[ none tp ].each do |scheme|
         conf = {
             schemes: [scheme],
             bench: $specint-%w[bzip2] }
         differing = compare_etime conf
         f = File.new( result_dir+"/etime_diff_#{scheme}.out", 'w' )
         differing.each{|f1,f2|f.puts "#{f1} differs from #{f2}"}
-        puts "Avg Cycle Difference:   " + ("%E" % (avg_difference conf) ).magenta
-        puts "Avg Percent Difference: " + (avg_percent conf).to_s.magenta
+        puts "#{scheme} Avg Cycle Difference:   " + ("%E" % (avg_difference conf) ).magenta
+        puts "#{scheme} Avg Percent Difference: " + (avg_percent conf).to_s.magenta
+        puts "#{scheme} Number of Differences:  " + (count_differences conf).to_s.magenta
+        puts "#{scheme} distribution:           " + (percent_diff_dist conf).to_s.magenta
         f.close()
     end
 
