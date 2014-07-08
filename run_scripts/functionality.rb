a367b5fa54432676d3585e4ba90aef1a2a075c2a
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2014-07-08 11:45:26 -0400

    Minor revisions to functionality tests

--- run_scripts/functionality.rb
+++ run_scripts/functionality.rb
@@ -7,120 +7,51 @@ require 'colored'
 include RunScripts
 
 module RunScripts
-    def sanity_ opts
-        opts = {
-            cpus: %w[ detailed ],
-            schemes: %w[ none ],
-            benchmarks: $specint - %w[bzip2],
-            maxinsts: 10**3,
-            fastforward: 0,
-            debug: true,
-        }.merge opts
-
-        parallel_local opts
+    def insecure opts={}
+     yield ({ 
+      cpus: %w[detailed],
+      schemes: %w[none],
+      benchmarks: $specint - %w[bzip2],
+      maxinsts: 10**3,
+      fastforward: 0,
+      debug: true,
+      runmode: :local,
+     }.merge opts)
     end
 
-    def sanity_deep_ opts
-        opts = {
-            cpus: %w[ detailed ],
-            benchmarks: %w[ mcf sjeng ],
-            maxinsts: 10**6,
-            fastforward: 0,
-            runmode: :local,
-            debug: true
-        }.merge opts
-
-        sanity_( opts )
+    def secure opts={}, &block
+      insecure ({
+        schemes: %w[tp],
+        waypart: true,
+        rr_nc: true,
+        addrpar: true,
+        split_rport: true,
+        split_mshr: true
+      }.merge opts){|args| block.call args}
     end
 
-    def sanity
-        sanity_( {} )
+    def secure_scaling
+      secure{|opts| parallel_local_scaling opts}
     end
 
-    def sanity_deep
-        sanity_deep_( {} )
+    def secure_sanity
+      secure{|opts| parallel_local opts}
     end
 
-    def secure
-        sanity_( {
-            schemes: %w[tp],
-            setpart: true,
-            rr_nc: true,
-            addrpar: true,
-            split_rport: true,
-            split_mshr: true,
-        } )
+    def insecure_sanity
+      insecure{|opts| parallel_local opts}
     end
-    ###########################################################################
-    # Currently fails:
-    # tp_detailed_gcctl6_astartl6_c4MB
-    ###########################################################################
 
     def secure_deep
-        sanity_deep_({
-            schemes: %w[tp],
-            setpart: true,
-            rr_nc: true,
-            addrpar: true,
-            split_rport: true,
-            split_mshr: true,
-        } )
+      secure{|opts| parallel_local opts.merge(
+        maxinsts: 10**6, fastforward: 100, benchmarks: %w[ mcf sjeng ]
+      )}
     end
 
-    def secure_wp
-        sanity_({
-            schemes: %w[tp],
-            waypart: true,
-            rr_nc: true,
-            addrpar: true,
-            split_rport: true,
-            split_mshr: true,
-        } )
-    end
-
-    def secure_wp_deep
-        sanity_deep_({
-            schemes: %w[tp],
-            aypart: true,
-            rr_nc: true,
-            addrpar: true,
-            split_rport: true,
-            split_mshr: true,
-        } )
-    end
-
-    def gcc_l2l3bus_hang
-        opts = {
-            nametag: "uncoordinated",
-            p1: "astar",
-            rr_nc: true,
-            addrpar: true,
-            setpart: true,
-            split_rport: true,
-            split_mshr: true,
-            runmode: :local,
-        }
-
-        #Coordinated Scheme
-        opts_coord = opts.merge({
-            nametag: "coordinated",
-            l2l3req_tl:        12,
-            l2l3req_offset:     0,
-            l2l3resp_tl:       12,
-            l2l3resp_offset:   12,
-            membusreq_tl:       1,
-            membusreq_offset:   0,
-            membusresp_tl:     96,
-            membusresp_offset: 49,
-            dramoffset:      -120,
-        })
-        Parallel.each(1..2, :in_threads=>2) do |i|
-            if i==1
-                sav_script( "detailed", "tp", "gcc", opts )
-            else
-                sav_script( "detailed", "tp", "gcc", opts_coord )
-            end
-        end
+    def insecure_deep
+      insecure{|opts| parallel_local opts.merge(
+        maxinsts: 10**6, fastforward: 100, benchmarks: %w[ mcf sjeng ]
+      )}
     end
 
 end
