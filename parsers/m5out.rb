a4717065129fcb81290737a4d03b747fdbbcb88a
Andrew <andrew@csl.cornell.edu>
2014-07-11 16:55:39 -0400

    Param sweeping parser

--- parsers/m5out.rb
+++ parsers/m5out.rb
@@ -1,12 +1,13 @@
 require_relative 'parsers'
 include Parsers
 
-$mem_latency = /system.l3.overall_avg_miss_latency::total (\d*.\d*)/
+module Parsers
+$mem_latency = /system.l3.overall_avg_miss_latency::total\s*(\d*.\d*)/
 $l3_miss = /system.l3.overall_misses::total[\s]*(\d*)/
 
 def get_stats( conf = {} )
     conf = {
-        bench: $specint-%w[bzip2],
+        bench: $specint,
         scheme: "none",
         cpu: "detailed",
     }.merge conf
@@ -14,9 +15,12 @@ def get_stats( conf = {} )
 
     conf[:bench].inject({}) {|hsh,p0|
         hsh[p0] = avg_arr(
-            conf[:otherbench].inject([]) {|p0stats,p1|
+            conf[:otherbench].inject([]) {|p0stats,other|
+                bench_specifier = block_given? ?
+                    yield(p0, other) :
+                    { p0: p0, p1: other }
                 datum, found = get_datum(
-                    m5out_file( conf.merge( p0:p0, p1:p1 ) ),
+                    m5out_file( conf.merge( bench_specifier ) ),
                     conf[:regex]
                 )
                 p0stats << datum.to_f if found
@@ -28,7 +32,7 @@ end
 
 def compare_stats( conf = {} )
     conf = {
-        bench: $specint-%w[bzip2],
+        bench: $specint,
         cpu: "detailed",
         scheme: "tp",
         base_scheme: "none",
@@ -37,8 +41,11 @@ def compare_stats( conf = {} )
 
     conf[:bench].inject({}) {|hsh,p0|
         hsh[p0] = avg_arr(
-            conf[:otherbench].inject([]) {|p0stats,p1|
-                p = conf.merge( p0:p0, p1: p1 )
+            conf[:otherbench].inject([]) {|p0stats,other|
+                bench_specifier = block_given? ?
+                    yield(p0, other) :
+                    { p0: p0, p1: other }
+                p = conf.merge( bench_specifier )
                 datum, found = get_datum(
                     m5out_file( p ),
                     conf[:regex]
@@ -47,17 +54,20 @@ def compare_stats( conf = {} )
                     m5out_file(
                         p.merge(
                             scheme: conf[:base_scheme],
-                            nametag: conf[:base_nametag]
+                            nametag: conf[:base_nametag],
+                            tl0: 6, tl1: 6, tl2: 6, tl3: 6
                         )
                     ),
                     conf[:regex]
                 )
-                p0stats << overhead(
-                    datum.to_f, datum_base.to_f ) if found && found_base
+                p0stats << overhead(datum.to_f, datum_base.to_f,
+                                   ) if found && found_base
                 p0stats
             }
-        ); hsh
+        ) ; hsh
     }
+end
+
 end
 
 if __FILE__ == $0
@@ -73,29 +83,42 @@ if __FILE__ == $0
     # puts "Baseline memory requests per thousand instructions: \n" + mpki.to_s
     # hash_to_csv( mpki, result_dir+"/mpki.csv" )
 
-    baseline_latency = get_stats( scheme: "none", dir: input_dir, regex: $mem_latency )
-    puts "Baseline Memory Latency: \n" + baseline_latency.to_s
-    hash_to_csv( baseline_latency, result_dir+"/baseline_latency.csv" )
+    # baseline_latency = get_stats( scheme: "none", dir: input_dir, regex: $mem_latency )
+    # puts "Baseline Memory Latency: \n" + baseline_latency.to_s
+    # hash_to_csv( baseline_latency, result_dir+"/baseline_latency.csv" )
 
-    # secure_latency = get_stats( scheme: "tp", dir: input_dir, regex: $mem_latency, otherbench: %w[astar] )
-    # puts "Secure Baseline Memory Latency: \n" + secure_latency.to_s
-    # hash_to_csv(  secure_latency, result_dir+"/secure_latency.csv" )
+    secure_latency = get_stats(
+        scheme: "tp",
+        dir: input_dir,
+        tl0: 64,
+        tl1: 64,
+        regex: $mem_latency,
+        otherbench: %w[astar]
+    )
+    puts "Secure Baseline Memory Latency: \n" + secure_latency.to_s
+    hash_to_csv(  secure_latency, result_dir+"/secure_latency.csv" )
 
-    latency_overhead = compare_stats( dir: input_dir, regex: $mem_latency, otherbench: %w[astar] )
-    puts "Baseline Latency Overhead: \n" + latency_overhead.to_s
-    hash_to_csv( latency_overhead, result_dir+"/baseline_overhead.csv" )
+    latency_overhead = compare_stats(
+        dir: input_dir,
+        tl0: 64,
+        tl1: 64,
+        regex: $mem_latency,
+        otherbench: %w[astar]
+    )
+    puts "Secure Baseline Latency Overhead: \n" + latency_overhead.to_s
+    hash_to_csv( latency_overhead, result_dir+"/sec_baseline_overhead.csv" )
 
-    latency_overhead = compare_stats( dir: input_dir,
-                                     nametag: "bad_coordinated",
-                                     regex: $mem_latency,
-                                     otherbench: %w[astar] )
-    puts "Bad Coordinated Latency Overhead: \n" + latency_overhead.to_s
-    hash_to_csv( latency_overhead, result_dir+"/bad_coordinated_overhead.csv" )
+    # latency_overhead = compare_stats( dir: input_dir,
+    #                                  nametag: "bad_coordinated",
+    #                                  regex: $mem_latency,
+    #                                  otherbench: %w[astar] )
+    # puts "Bad Coordinated Latency Overhead: \n" + latency_overhead.to_s
+    # hash_to_csv( latency_overhead, result_dir+"/bad_coordinated_overhead.csv" )
 
-    latency_overhead = compare_stats( dir: input_dir,
-                                     nametag: "coordinated",
-                                     regex: $mem_latency,
-                                     otherbench: %w[astar] )
-    puts "Coordinated Latency Overhead: \n" + latency_overhead.to_s
-    hash_to_csv( latency_overhead, result_dir+"/coordinated_overhead.csv" )
+    # latency_overhead = compare_stats( dir: input_dir,
+    #                                  nametag: "coordinated",
+    #                                  regex: $mem_latency,
+    #                                  otherbench: %w[astar] )
+    # puts "Coordinated Latency Overhead: \n" + latency_overhead.to_s
+    # hash_to_csv( latency_overhead, result_dir+"/coordinated_overhead.csv" )
 end
--- parsers/param_sweep.rb
+++ parsers/param_sweep.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/ruby
 require 'colored'
 require_relative 'parsers'
+require_relative 'm5out'
 include Parsers
 
 def param_sweeping opts
@@ -9,10 +10,15 @@ def param_sweeping opts
     #build a hash from parameter values to the performance comparison with the
     #baseline
     range.inject({}) { |h, v|
-        #get a hash to specify the name
-        h[v] = perf_compare( opts ) do |p0, other|
-            block_given? ? yield(p0, other, v) : {}
-        end
+        #get a hash of the form: parameter->benchmark->overhead
+
+        h[v] = opts[:regex].nil? ?
+            perf_compare( opts ) {|p0, other|
+                block_given? ? yield(p0, other, v) : {}
+            } :
+            compare_stats( opts ){|p0, other|
+                block_given? ? yield(p0, other, v): {}
+            }
         h
     }
 end
@@ -25,14 +31,14 @@ def l2l3_sweep opts={}
     two_threads = param_sweeping(opts) do |p0, other, v|
         { p0: p0, p1: other, nametag: "l2l3resp_#{v}" }
     end
-    # three_threads = param_sweeping(opts) do |p0, other, v|
-    #     { p0: p0, p1: other, p2: other, nametag: "l2l3resp_#{v}" }
-    # end
-    # four_threads = param_sweeping(opts) do |p0, other, v|
-    #     { p0: p0, p1: other, p2: other, p3: other,
-    #       nametag: "l2l3resp_#{v}" }
-    # end
-    #[ two_threads, three_threads, four_threads #]
+    three_threads = param_sweeping(opts) do |p0, other, v|
+        { p0: p0, p1: other, p2: other, nametag: "l2l3resp_#{v}" }
+    end
+    four_threads = param_sweeping(opts) do |p0, other, v|
+        { p0: p0, p1: other, p2: other, p3: other,
+          nametag: "l2l3resp_#{v}" }
+    end
+    [two_threads, three_threads, four_threads ]
 end
 
 def membus_sweep opts={}
@@ -43,15 +49,15 @@ def membus_sweep opts={}
     two_threads = param_sweeping(opts) do |p0, other, v|
         { p0: p0, p1: other, nametag: "membusresp_#{v}" }
     end
-    # three_threads = param_sweeping(opts) do |p0, other, v|
-    #     { p0: p0, p1: other, p2: other,
-    #       nametag: "membusresp_#{v}" }
-    # end
-    # four_threads = param_sweeping(opts) do |p0, other, v|
-    #     { p0: p0, p1: other, p2: other, p3: other,
-    #       nametag: "membusresp_#{v}" }
-    # end
-    # [ two_threads, three_threads, four_threads ]
+    three_threads = param_sweeping(opts) do |p0, other, v|
+        { p0: p0, p1: other, p2: other,
+          nametag: "membusresp_#{v}" }
+    end
+    four_threads = param_sweeping(opts) do |p0, other, v|
+        { p0: p0, p1: other, p2: other, p3: other,
+          nametag: "membusresp_#{v}" }
+    end
+    [ two_threads, three_threads, four_threads ]
 end 
 
 def tp_sweep opts={}
@@ -63,52 +69,92 @@ def tp_sweep opts={}
         { p0: p0, p1: other,
           tl0: v, tl1: v }
     end
-    # three_threads = param_sweeping(opts) do |p0, other, v|
-    #     { p0: p0, p1: other, p2: other,
-    #       tl0: v, tl1: v, tl2: v }
-    # end
-    # four_threads = param_sweeping(opts) do |p0, other, v|
-    #     { p0: p0, p1: other, p2: other, p3: other,
-    #       tl0: v, tl1: v, tl2: v }
-    # end
-    # [ two_threads, three_threads, four_threads ]
+    three_threads = param_sweeping(opts) do |p0, other, v|
+        { p0: p0, p1: other, p2: other,
+          tl0: v, tl1: v, tl2: v }
+    end
+    four_threads = param_sweeping(opts) do |p0, other, v|
+        { p0: p0, p1: other, p2: other, p3: other,
+          tl0: v, tl1: v, tl2: v, tl3: v }
+    end
+    [ two_threads, three_threads, four_threads ]
 end 
 
 def fa_sweep opts = {}
     tp_sweep opts.merge( scheme: "fa" )
 end
 
-def results_to_stdout results
+def results_to_s results
   #print heach benchmark
-  puts $specint.inject("") { |s, b| s+=b+", " }[0..-2]
+  r = $specint.inject("") { |s, b| s+=b+", " }[0..-2] +"\n"
   #for each paramter value, print the overhead
   results.each do |k,v|
-    puts "#{k}" + v.inject("") { |s,pair| s+=", "+pair[1].to_s }
+    r += "#{k}" + v.inject("") { |s,pair| s+=", "+pair[1].to_s }
   end
+  r
 end
 
-def results_to_stdout_t results
+def results_to_s_t results
   #print pramater values
-  puts "%-14s" % ("bench,") + results.keys.inject("") { |s, v|
+  ret = "%-14s" % ("bench,") + results.keys.inject("") { |s, v|
     s += "%-14s" % (v.to_s + ", ")
-  }
+  } + "\n"
   $specint.each do |b|
-    puts "%-14s" % (b + ",") + results.values.inject("") { |s, r|
+    ret += "%-14s" % (b + ",") + results.values.inject("") { |s, r|
       s+= "%-14s" % (("%.4f" % r[b]) + ",")
-    }
+    } + "\n"
   end
+  ret
+end
+
+def string_to_f string, filename
+    File.open(filename, 'w') { |f| f.puts string }
 end
 
 
 if __FILE__ == $0
-    puts "l2l3 sweeping:"
-    results_to_stdout_t l2l3_sweep(
-        #otherbench: %w[astar],
-    )
-    puts ""
+    out = ARGV[0].to_s
+    FileUtils.mkdir_p(out) unless File.directory?(out)
 
-    puts "fa sweeping:"
-    results_to_stdout_t fa_sweep(
-        #otherbench: %w[astar]
-    )
+    # Output results
+    main = lambda do |opts|
+     [2,3,4].each do |num_tcids|
+      puts "="*80
+      puts "   #{num_tcids} TCIDS"
+      puts "="*80
+
+      puts "l2l3 #{opts[:label]}:"
+      r = results_to_s_t l2l3_sweep(opts)[num_tcids-2]
+      puts r
+      string_to_f r, out+"/l2l3_#{opts[:label]}_#{num_tcids}.csv"
+      puts ""
+
+      puts "membus #{opts[:label]}:"
+      r = results_to_s_t membus_sweep(opts)[num_tcids-2]
+      puts r
+      string_to_f r, out+"/membus_#{opts[:label]}_#{num_tcids}.csv"
+      puts ""
+
+      puts "tp #{opts[:label]}:"
+      r = results_to_s_t tp_sweep(opts)[num_tcids-2]
+      puts r
+      string_to_f r, out+"/tp_#{opts[:label]}_#{num_tcids}.csv"
+      puts ""
+
+      puts "fa #{opts[:label]}:"
+      r = results_to_s_t fa_sweep(opts)[num_tcids-2]
+      puts r
+      string_to_f r, out+"/fa_#{opts[:label]}_#{num_tcids}.csv"
+      puts ""
+     end
+    end
+
+    # Execution Time Overhead
+    # main.call(otherbench: %w[mcf], label: "execution")
+
+    # L3 Miss Latency Overhead
+    main.call(otherbench: %w[astar],
+              regex: $mem_latency,
+         label: "latency_overhead")
+
 end
