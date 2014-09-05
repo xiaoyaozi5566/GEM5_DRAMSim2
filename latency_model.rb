862eea4dd45ac0e0c05fef61f0b7eb57a4184c72
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2014-09-05 19:57:23 -0400

    Latency Model can get EV / range

--- latency_model.rb
+++ latency_model.rb
@@ -90,7 +90,6 @@ class MemoryController
       t_mem:       64, # total turn length, not when data is available
       wc:          43,
       wc_read:     24, # tRCD + tCAS + WL/2
-      flight_time: 43  # time required to 
     }.merge o
 
     @name = o[:name]
@@ -104,7 +103,7 @@ class MemoryController
     @slots = [:active] * (o[:t_mem] - dead_time) +
       [:dead] * dead_time +
       [:other] * o[:t_mem] * (o[:ntcs] - 1)
-    @slots.rotate!(o[:offset])
+    @slots.rotate!(-o[:offset])
   end
 
   @@memcs = {}
@@ -143,8 +142,7 @@ class MemoryController
   def to_s
     "(#{"%4d" % @time}) " + "%10s" % @name +
       (SHIFT ? " " *  @time : "") +
-      " #{@slots.inject(""){ |s,slot| s += sym_to_s slot }}" +
-      "#{@slots.inject(""){ |s,slot| s += sym_to_s slot }}"
+      " #{@slots.inject(""){ |s,slot| s += sym_to_s slot }}"
   end
 
 end
@@ -195,7 +193,7 @@ def simulate_l3_hit o = {}
   end
 end
 
-def simulate_l3_miss o = {}
+def l3_hit_latencies o={}
   o = {
     # System Configuration
     l2l3req_tl:   1,
@@ -207,9 +205,44 @@ def simulate_l3_miss o = {}
     l3memresp_tl: 9,
     l3memresp_o:  0,
     # Environment
-    t_l3:  9,
+    t_l3:   9,
     t_req:  1,
     t_resp: 9,
+    ntcs:   2,
+  }.merge o
+
+  ([
+    o[:l2l3req_tl],
+    o[:l2l3resp_tl],
+  ].reduce(:lcm) * o[:ntcs]).times.to_a.map do |time|
+    puts ("="*80).blue if DEBUG
+    puts (" "*40 + "#{time}").blue if DEBUG
+    puts ("="*80).blue if DEBUG
+    latency = simulate_l3_hit(o.merge(time: time)) - time
+    puts latency.to_s.blue if DEBUG
+    latency
+  end
+end
+
+def simulate_l3_miss o = {}
+  o = {
+    # System Configuration
+    l2l3req_tl:   1,
+    l2l3req_o:    0,
+    l2l3resp_tl:  9,
+    l2l3resp_o:   0,
+    l3memreq_tl:  1,
+    l3memreq_o:   0,
+    l3memresp_tl: 9,
+    l3memresp_o:  0,
+    mem_tl:      64,
+    mem_o:        0,
+    # Environment
+    t_l3:   9,
+    t_req:  1,
+    t_resp: 9,
+    mem_wc: 43,
+    mem_wc_read: 24,
     # Test case
     ntcs:   2,
     time:   0,
@@ -235,11 +268,12 @@ def simulate_l3_miss o = {}
       offset:  o[:l3memreq_o],
     ),
     MemoryController.new_m(
-      name: "MemCtl",
-      ntcs: o[:ntcs],
-      # t_mem: o[:t_mem],
-      # offset: o[:mem_offset],
-      #other env params
+      name:    "MemCtl",
+      wc:      o[:mem_wc],
+      wc_read: o[:mem_wc_read],
+      ntcs:    o[:ntcs],
+      t_mem:   o[:mem_tl],
+      offset:  o[:mem_o]
     ),
     BusLayer.new_m(
       name:    "l3mem_resp",
@@ -248,7 +282,7 @@ def simulate_l3_miss o = {}
       turn_l:  o[:l3memresp_tl],
       offset:  o[:l3memresp_o],
     ),
-    l3,
+    l3, # <-- same L3 as above
     BusLayer.new_m(
       name:    "l2l3_resp",
       message: o[:t_resp],
@@ -258,13 +292,55 @@ def simulate_l3_miss o = {}
     )
   ].inject(o[:time]) do |time, component|
     component.update time #happens automatically, needed for print
-    puts component.to_s.green if DEBUG
+    puts component.to_s.green if DEBUG && BOXES
     time = component.cycle_complete time
   end
 end
 
+def l3_miss_latencies o={}
+  o = {
+    # System Configuration
+    l2l3req_tl:   1,
+    l2l3req_o:    0,
+    l2l3resp_tl:  9,
+    l2l3resp_o:   0,
+    l3memreq_tl:  1,
+    l3memreq_o:   0,
+    l3memresp_tl: 9,
+    l3memresp_o:  0,
+    mem_tl:      64,
+    mem_o:        0,
+    # Environment
+    t_l3:    9,
+    t_req:   1,
+    t_resp:  9,
+    mem_wc: 43,
+    ntcs:    2,
+  }.merge o
+
+  ([
+    o[:l2l3req_tl],
+    o[:l2l3resp_tl],
+    o[:l3memreq_tl],
+    o[:l3memresp_tl],
+    o[:mem_tl]
+  ].reduce(:lcm) * o[:ntcs]).times.to_a.map do |time|
+    puts ("="*80).blue if DEBUG
+    puts (" "*40 + "#{time}").blue if DEBUG
+    puts ("="*80).blue if DEBUG
+    latency = simulate_l3_miss(o.merge(time: time)) - time
+    puts latency.to_s.yellow if DEBUG
+    latency
+  end
+end
+
+def avg arr
+  arr.reduce(:+)/arr.length.to_f
+end
+
 if __FILE__ == $0
   DEBUG = true
+  BOXES = false
   SHIFT = true 
   # a = SlotBusLayer.new(ntcs: 4, offset: 0, turn_l: 3, name: "l2l3_req")
   # puts a.cycle_complete 0, 3
@@ -286,6 +362,22 @@ if __FILE__ == $0
   #   l2l3resp_o: 1+9 #t_req + t_l3
   # ).to_s.magenta
 
-  puts simulate_l3_miss.to_s.magenta
+  # puts simulate_l3_miss(
+  #   l2l3resp_o: 10,
+  #   l3memreq_o: 10,
+  #   mem_o:      11,
+  #   l3memresp_o: 11+24
+  # ).to_s.magenta
+ 
+  hit_latencies = l3_miss_latencies(
+    l3memreq_o:        10,
+    mem_o:             11,
+    l3memresp_o:    11+24,
+    l2l3resp_o:   11+24+9,
+    mem_tl:            64,
+  ) 
+
+  puts "|L| = #{avg hit_latencies}".to_s.magenta
+  puts "#{hit_latencies.max } < L < #{hit_latencies.min}".to_s.magenta
 
 end
