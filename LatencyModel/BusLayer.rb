ef437dcf5034d4720273a504c7fabd67d99d73f1
Andrew Ferraiuolo <andrew@csl.cornell.edu>
2014-09-05 20:12:25 -0400

    Reorganize Latency Model

---
+++ LatencyModel/BusLayer.rb
@@ -0,0 +1,53 @@
+Dir["*rb"].each { |f| require_relative f }
+
+class BusLayer
+
+  def initialize o = {}
+  
+    o = {
+      ntcs: 2, offset: 0, turn_l: 1,
+      name: "", message: 1
+    }.merge o
+    @message_length  = o[:message]
+    @name = o[:name]
+
+    @time = 0
+
+    @slots = [true] * o[:turn_l] +
+      [false] * o[:turn_l] * (o[:ntcs] - 1)
+    @slots = @slots.rotate(-o[:offset])
+  end
+
+  #Memoized Constructor
+  @@buses = {}
+  def self.new_m o
+    @@buses[o] ||= self.new o
+  end
+
+  # Calculate when the data for a packet entering at "time" will be available
+  # based on the parameters from initializaiton
+  def cycle_complete time
+    update time
+
+    @message_length.times do
+      update(@time + 1) while !@slots[0]
+      update(@time + 1)
+    end
+
+    @time
+  end
+
+  def update time
+    @slots.rotate!(time-@time)
+    @time = time
+  end
+
+  WHITESQ = "\u25A1"
+  BLKSQ   = "\u25A0"
+  def to_s
+    "(#{"%4d" % @time}) " + "%10s" % @name +
+      (SHIFT ? " " *  @time : "") +
+      " #{@slots.inject(""){ |s,slot| s += slot ? BLKSQ : WHITESQ }}"
+  end
+  
+end
---
+++ LatencyModel/PipelinedCache.rb
@@ -0,0 +1,30 @@
+Dir["*rb"].each { |f| require_relative f }
+
+class PipelinedCache
+  WHITESQ = "\u25A0"
+  def initialize o = {}
+    o = {
+      name: "L3",
+      t_l3: 9
+    }.merge o
+    @name = o[:name]
+    @t_l3 = o[:t_l3]
+    @t    = 0
+  end 
+
+  #Memoized Constructor
+  @@caches = {}
+  def self.new_m o
+    @@caches[o] ||= self.new(o)
+  end
+
+  def cycle_complete(t) @t = t + @t_l3 end
+  def update(t) @t = t end
+  def self.new_m(o) new(o) end
+
+  def to_s
+    "(#{"%4d" % @t}) " + "%10s" % @name +
+    (SHIFT ? " " * (1+@t) : " ") +
+    WHITESQ * @t_l3
+  end
+end
